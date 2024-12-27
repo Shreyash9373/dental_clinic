1a7f2e0e7d6acac1261837c6ecb85a7d7abeda35
smit <smitbharshankar110@gmail.com>
2024-12-27 18:02:34 +0530

    patient displaydoctors and floatingcontact

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Patient from './components/Patient';
+import DisplayDoctor from './components/DisplayDoctor';
+import WhatsappContact from './components/whatsappContact';
 
 const App = () => {
-  return (
-    <div className='text-red-500'>
-      hello
-    </div>
-  )
-}
+ 
 
-export default App
+  return (
+    
+    <div className='w-full h-auto overflow-hidden'>
+      <div>
+        <WhatsappContact />
+      </div>
+      <Patient />
+      <div className="mt-8 mx-2">
+        <DisplayDoctor />
+      </div>
+    </div>
+  );
+};
+
+export default App;
