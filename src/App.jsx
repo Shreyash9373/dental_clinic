22529bb05e18f654770a3cc1ae733571706893b1
Akshay <kedarakshay1555@gmail.com>
2024-12-28 12:57:12 +0530

    changes made by Akshay

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Service from "./pages/Service.jsx";
 
-const App = () => {
+function App() {
   return (
-    <div className='text-red-500'>
-      hello
-    </div>
-  )
+    <Router>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Service from "./Service.jsx"
+
+const Home = () => {
+  return (
+    <>
+      <Service/>
+    </>
+  );
+};
+
+export default Home;
