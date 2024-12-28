e2917cb5f0656d71b39949b3f8a8c96c65f325d4
Aniket <anikettambe1509@gmail.com>
2024-12-28 12:35:49 +0530

    All changes made by Aniket

---
+++ src/components/SmallFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SmallFooter = () => {
+  return (
+    <div className="p-5 text-center text-gray-400 text-xl bg-gray-800">
+      Copyright © 2024. Dr. Pakhare Dental Clinic. Developed by Peakprosys Solutions Pvt. Ltd. .
+    </div>
+  )
+}
+
+export default SmallFooter
